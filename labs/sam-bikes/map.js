ea18a691c7fce3c7005104530f9e57dcba1530c5
Sam Lau <samlau95@gmail.com>
2025-02-20 19:56:31 -0800

    WIP

--- labs/sam-bikes/map.js
+++ labs/sam-bikes/map.js
@@ -59,33 +59,6 @@ map.on('load', async () => {
 
   let stations = jsonData.data.stations;
 
-  const svg = d3.select('#map').select('svg');
-  const circles = svg
-    .selectAll('circle')
-    .data(stations)
-    .enter()
-    .append('circle')
-    .attr('r', 5) // Radius of the circle
-    .attr('fill', 'steelblue') // Circle fill color
-    .attr('stroke', 'white') // Circle border color
-    .attr('stroke-width', 1) // Circle border thickness
-    .attr('opacity', 0.8); // Circle opacity
-
-  function updatePositions() {
-    circles
-      .attr('cx', (d) => getCoords(d).cx) // Set the x-position using projected coordinates
-      .attr('cy', (d) => getCoords(d).cy); // Set the y-position using projected coordinates
-  }
-
-  // Initial position update when map loads
-  updatePositions();
-
-  // Reposition markers on map interactions
-  map.on('move', updatePositions); // Update during map movement
-  map.on('zoom', updatePositions); // Update during zooming
-  map.on('resize', updatePositions); // Update on window resize
-  map.on('moveend', updatePositions); // Final adjustment after movement ends
-
   const trips = await d3.csv(
     'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv',
   );
@@ -110,7 +83,45 @@ map.on('load', async () => {
     return station;
   });
 
-  console.log(departures);
+  const radiusScale = d3
+    .scaleSqrt()
+    .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+    .range([0, 25]);
+
+  const svg = d3.select('#map').select('svg');
+  const circles = svg
+    .selectAll('circle')
+    .data(stations)
+    .enter()
+    .append('circle')
+    .attr('r', (d) => radiusScale(d.totalTraffic)) // Radius of the circle
+    .attr('fill', 'steelblue') // Circle fill color
+    .attr('stroke', 'white') // Circle border color
+    .attr('stroke-width', 1) // Circle border thickness
+    .attr('opacity', 0.6) // Circle opacity
+    .each(function (d) {
+      // Add <title> for browser tooltips
+      d3.select(this)
+        .append('title')
+        .text(
+          `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`,
+        );
+    });
+
+  function updatePositions() {
+    circles
+      .attr('cx', (d) => getCoords(d).cx) // Set the x-position using projected coordinates
+      .attr('cy', (d) => getCoords(d).cy); // Set the y-position using projected coordinates
+  }
+
+  // Initial position update when map loads
+  updatePositions();
+
+  // Reposition markers on map interactions
+  map.on('move', updatePositions); // Update during map movement
+  map.on('zoom', updatePositions); // Update during zooming
+  map.on('resize', updatePositions); // Update on window resize
+  map.on('moveend', updatePositions); // Final adjustment after movement ends
 });
 
 function getCoords(station) {
